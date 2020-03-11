117723cae2a2f86313bb8977f6b5f72c382a3f85
creinosolt <creinoso@lean-tech.io>
2020-03-10 23:48:30 -0500

    progress until video 14, add mongoose to save some user data after google login to a mongo database, add mongodb uri to the keys file, add user model to create a new mongodb schema, reinstall nodemon, validation if the user is registered it will retrieve the actual user data

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,12 +1,25 @@
 const passport = require('passport')
 const keys = require('./keys')
 const GoogleStrategy = require('passport-google-oauth20')
+const User = require('../models/user.model')
 
 passport.use(new GoogleStrategy({
     callbackURL: '/auth/google/redirect',
     clientID: keys.google.clientID,
     clientSecret: keys.google.clientSecret
 }, (accessToken, refreshToken, profile, done) => {
-    console.log('passport callback function fired!!!!!')
     console.log(profile)
+    User.findOne({ googleId: profile.id }).then((currentUser) => {
+        if (currentUser) {
+            console.log(`User ${profile.displayName} with ID:${profile.id} is already in the DB.`)
+        } else {
+            new User({
+                username: profile.displayName,
+                googleId: profile.id
+            }).save().then((newUser) => {
+                console.log('New User: ', newUser)
+            })
+        }
+    })
+
 }))
--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const authRoutes = require('./routes/auth.routes')
 const passportSetup = require('./config/passport-setup')
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
 
 const app = express()
 const PORT = process.env.PORT || 2000
+const mongoOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
 
 app.set('view engine', 'ejs')
+
+mongoose.connect(keys.mongodb.dbUri, mongoOptions, () => {
+    console.log('Connected to MongoDB.')
+})
 
 app.use('/auth', authRoutes)
 
